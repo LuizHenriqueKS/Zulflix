d3bc6e0e3d38e161d64206bf5c985f000d9fbc84
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2020-07-30 18:15:09 -0300

    Update index.js

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -20,7 +20,7 @@ function CadastroCategoria() {
   document.getElementById('root').setAttribute('component', 'CadastroCategoria');
 
   useEffect(() => {
-    const URL_TOP = window.localtion.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'http://localhost:8080/categorias';
+    const URL_TOP = window.localtion.hostname.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://zulflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => respostaDoServidor.json())
       .then(async (respostaConvertidaEmObj) => setCategorias([...respostaConvertidaEmObj]));
